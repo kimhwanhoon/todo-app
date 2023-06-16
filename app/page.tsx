9459d41e4466e6f0b8c5b6afa4aed5a9f975ec48
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-06-16 10:31:35 +0900

    version 1.0.6 main width adjusted so that there can always be margin left and right some much.

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Main from './main.jsx';
 
 export default function Home() {
   return (
-    <div id="CONTAINER" className="flex flex-col mx-auto my-5 max-w-1200">
-      <Header />
-      <main className="rounded-b-xl w-full bg-slate-50 h-[calc(100vh-120px)] flex flex-col items-center overflow-auto">
-        <Main />
-      </main>
+    <div className="flex justify-center">
+      <div id="CONTAINER" className="flex w-full flex-col mx-8 my-5 max-w-1200">
+        <Header />
+        <main className="rounded-b-xl w-full bg-slate-50 h-[calc(100vh-120px)] flex flex-col items-center overflow-auto">
+          <Main />
+        </main>
+      </div>
     </div>
   );
 }
